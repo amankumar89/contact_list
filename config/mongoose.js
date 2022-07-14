8be0a49e35135b20020489d6b4c4eff149c13c12
Aman Kumar <amankumaroo784@gmail.com>
2022-07-14 10:40:41 +0530

    add codes related to contact_list nodejs express

---
+++ config/mongoose.js
@@ -0,0 +1,16 @@
+// require the library
+const mongoose = require("mongoose");
+
+// connect to the database
+mongoose.connect("mongodb://localhost/contact_list_db");
+
+// aquire the connection
+const db = mongoose.connection;
+
+// error
+db.on("error", console.error.bind(console, "connection error:"));
+
+// up and running
+db.once("open", function () {
+  console.log("Successfully connected to database");
+});
