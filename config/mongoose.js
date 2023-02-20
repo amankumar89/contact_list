70269deebe766110b21687f7d9a2b480f90b44c4
Aman Kumar <amankumaroo784@gmail.com>
2023-02-20 10:15:26 +0530

    fixed database port error

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,16 +1,16 @@
 // require the library
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 // connect to the database
-mongoose.connect("mongodb://localhost/contact_list_db");
+mongoose.connect('mongodb://0.0.0.0/contact_list_db');
 
 // aquire the connection
 const db = mongoose.connection;
 
 // error
-db.on("error", console.error.bind(console, "connection error:"));
+db.on('error', console.error.bind(console, 'connection error:'));
 
 // up and running
-db.once("open", function () {
-  console.log("Successfully connected to database");
+db.once('open', function () {
+  console.log('Successfully connected to database');
 });
