dacbf423f03d7f3ae8b429e29b5b7b170bd8ec12
amankumar89 <amankumaroo784@gmail.com>
2024-01-27 13:05:31 +0530

    added mongodb atlas

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,16 +1,14 @@
 // require the library
 const mongoose = require('mongoose');
 
-// connect to the database
-mongoose.connect('mongodb://0.0.0.0/contact_list_db');
+// uri
+const uri = 'mongodb+srv://amankumaroo784:amank784@cluster0.3ba1r6o.mongodb.net/?retryWrites=true&w=majority';
 
-// aquire the connection
-const db = mongoose.connection;
-
-// error
-db.on('error', console.error.bind(console, 'connection error:'));
-
-// up and running
-db.once('open', function () {
-  console.log('Successfully connected to database');
-});
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => {
+  console.log('MongoDB Connected')
+})
+.catch(err => console.log(err))
